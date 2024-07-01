07f50fb1257c880f6c524cb5908a98261f537411
vracto <115945845+vracton@users.noreply.github.com>
2024-06-30 23:48:01 -0500

    polygon + adjust point to coordinate system

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ let h
 let stroke = "#000"
 let size = 3
 let detail = 100
+let fill = "#000"
 
 function drawGraph(x, y){
   w=x*20
@@ -41,6 +42,10 @@ function setColor(c){
   stroke=c
 }
 
+function setFill(f){
+  fill = f
+}
+
 function setSize(s){
   size=s
 }
@@ -52,20 +57,43 @@ function drawEq(func){
 function drawDashedEq(func, dashLen, space){
   const pLine = bt.copy(bt.catmullRom(generateEq(func)[0], detail))
   for (let i = 0;i < pLine.length;i+=(dashLen+space)){
-    drawLines([[pLine[i],pLine[i+dashLen]]],{width:size,stroke:stroke})
+    drawLines(bt.copy([[[pLine[i],pLine[i+dashLen]]]])[0],{width:size,stroke:stroke})
   }
 }
 
-function drawPoint(x,y){
+function drawPoint(xPos,yPos){
+  const x = xPos+w/2
+  const y = yPos+h/2
   drawLines([bt.catmullRom([[x-0.1,y-0.1],[x+0.1,y-0.1],[x+0.1,y+0.1],[x-0.1,y+0.1],[x-0.1,y-0.1]])],{width:15,stroke:stroke,fill:stroke})
 }
 
-//<-------------------------------------------------->
+function drawPolygon(filled){
+  let pLine = [[[]]]
+  for (let i=1;i<arguments.length;i++){
+    if (pLine[0][0][pLine[0][0].length-1]&&pLine[0][0][pLine[0][0].length-1].length==1){
+      pLine[0][0][pLine[0][0].length-1][1]=arguments[i]+h/2
+    } else {
+      pLine[0][0].push([arguments[i]+w/2])
+    }
+  }
+  if (pLine[0][0].length>0){
+    if (pLine[0][0][pLine[0][0].length-1].length==1){
+      pLine[0][0].pop()
+    }
+    console.log(pLine)
+    pLine[0][0].push(pLine[0][0][0])
+    drawLines(pLine[0],{width:size,stroke:stroke,fill:(filled?fill:"#00000000")})
+  }
+}
+
+//-------------------------------------------------------------------------
 
 drawGraph(10,10)
 
+setFill("#000ff050")
+drawPolygon(true,0,0,100,0,100,100)
 setColor("#BF40BF")
-drawPoint(50,150)
+drawPoint(50,50)
 setColor("#00ff00")
 drawEq(x=>x, 1000, 1000)
 setSize(5)
